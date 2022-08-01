d6ec8aeb1336154c1b5fba39816fdad8b9e3623f
Michelle Hapich <mhapich@gmail.com>
2022-08-01 16:16:21 -0400

    used CYBORG style sheet

--- fifa_dash.py
+++ fifa_dash.py
@@ -12,24 +12,18 @@ fifa_df = pd.read_csv('fifa_soccer_players.csv')
 app = Dash(external_stylesheets=[dbc.themes.CYBORG])
 
 app.layout = html.Div([
-    html.H1('Soccer Players Dashboard',
-             style={'textAlign':'center',
-                    'fontFamily':'Georgia',
-                    'color':'blue',
-                    'fontSize':'50px'}),
-    html.P(['Source: ',
-             html.A('Sofifa',
+    html.H1('Soccer Players Dashboard'),
+    dbc.Row([dbc.Col(html.P(['Source: ',
+                     html.A('Sofifa',
                      href='https://sofifa.com',
-                     target='_blank')],
-            style={'border':'solid'}),
-    html.Label('Player name: '),
-    dcc.Dropdown(id='pname-dd',
-                 options=fifa_df['long_name'].unique(),
-                 value=fifa_df['long_name'].unique()[0],
-                 style={'backgroundColor':'lightblue'})
-],
-
-style={'padding':'100px', 'border':'solid'})
+                     target='_blank')])),
+            dbc.Col([html.Label('Player name: '),
+                     dcc.Dropdown(id='pname-dd',
+                     options=fifa_df['long_name'].unique(),
+                     value=fifa_df['long_name'].unique()[0])
+            ])
+            ])    
+])
 
 if __name__=='__main__':
     app.run_server(debug=True)
