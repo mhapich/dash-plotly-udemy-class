fb0e4b2d1f91915dabc1e11f4b027ed429d0bab1
Michelle Hapich <mhapich@gmail.com>
2022-08-01 21:58:35 -0400

    finished fifa_dash2 using cards

--- fifa_dash2.py
+++ fifa_dash2.py
@@ -7,6 +7,11 @@ import dash_bootstrap_components as dbc
 # read in dataset
 soccer = pd.read_csv('fifa_soccer_players.csv')
 
+# calculate statistics for cards
+avg_age = soccer['age'].mean()
+avg_height = soccer['height_cm'].mean()
+avg_weight = soccer['weight_kg'].mean()
+
 # set up navbar
 navbar = dbc.NavbarSimple(
     brand='Soccer Players Dashboard',
@@ -22,12 +27,33 @@ navbar = dbc.NavbarSimple(
     fluid=True
 )
 
+# create cards to be used in layout
+cards = dbc.Row([dbc.Col(dbc.Card([
+                                html.H4('Average Age'),
+                                html.H5(f'{round(avg_age, 1)} years')],
+                                body=True,
+                                style={'textAlign':'center', 'color':'white'},
+                                color='lightblue')),
+                 dbc.Col(dbc.Card([
+                                html.H4('Average Height'),
+                                html.H5(f'{round(avg_height, 1)} cm')],
+                                body=True,
+                                style={'textAlign':'center', 'color':'white'},
+                                color='blue')),
+                 dbc.Col(dbc.Card([
+                                html.H4('Average Weight'),
+                                html.H5(f'{round(avg_weight, 1)} kg')],
+                                body=True,
+                                style={'textAlign':'center', 'color':'white'},
+                                color='darkblue'))
+])
+
 # instantiate Dash app using bootstrap components
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 
 # set up page layout
-app.layout = html.Div(navbar)
+app.layout = html.Div([navbar, html.Br(), cards])
 
 # set up callback area
 
