87d10673da29a41670ee0d128eedd1871eb763fd
Michelle Hapich <mhapich@gmail.com>
2022-08-03 19:41:50 -0400

    added timing elements; before table works again

--- stocks_dash.py
+++ stocks_dash.py
@@ -1,14 +1,9 @@
-from dash import Dash, html, dcc, dash_table
+from dash import Dash, html, dcc, dash_table, Input, Output, State
 import yfinance as yf
 import plotly.graph_objects as go
 
-price=yf.Ticker('AAPL').history(period='1d', interval='15m').reset_index()
-# create graph here for layout
-fig = go.Figure(data=go.Candlestick(x=price['Datetime'],
-                open=price['Open'],
-                high=price['High'],
-                low=price['Low'],
-                close=price['Close']))
+
+price=yf.Ticker(' ').history(period='1d', interval='15m').reset_index()
 
 app = Dash()
 
@@ -22,13 +17,39 @@ app.layout = html.Div([
     html.Br(),
     dcc.Tabs([
         dcc.Tab(label='Candlestick Chart', 
-                children=dcc.Graph(id='stock-graph', figure=fig)),
+                children=dcc.Graph(id='stock-graph')),
         dcc.Tab(label = 'Recent Data', 
                 children=dash_table.DataTable(
                     id='stock-data', 
-                    data=price.tail(10).to_dict('records')))
-    ])
+                    data=price.tail(10).to_dict('records'))),
+    ]),
+    dcc.Interval(id='chart-interval', interval=1000*60*15, n_intervals=0),
+    dcc.Interval(id='table-interval', interval=1000*60, n_intervals=0)
 ])
+
+@app.callback(
+    # the graph will be updated
+    Output('stock-graph', 'figure'),
+    # by either the submit button being clicked or time passing
+    Input('submit-button', 'n_clicks'), 
+    Input('chart-interval', 'n_intervals'),
+    # the chosen stock symbol will be held as a state until a trigger is made
+    State('ticker-input', 'value')
+)
+
+def update_chart(button_click, chart_interval, stock_symbol):
+    if stock_symbol is None:
+        return {}
+    price=yf.Ticker(stock_symbol).history(period='1d', interval='15m').reset_index()
+    if len(price) == 0:
+        return {}
+    # create graph here for layout
+    fig = go.Figure(data=go.Candlestick(x=price['Datetime'],
+                open=price['Open'],
+                high=price['High'],
+                low=price['Low'],
+                close=price['Close']))  
+    return fig
 
 if __name__=='__main__':
     app.run_server(debug=True)
