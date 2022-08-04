26f888c0602e002b9542b4f91c7f1ce76d2aa018
Michelle Hapich <mhapich@gmail.com>
2022-08-04 09:14:15 -0400

    finished stocks_dash

--- stocks_dash.py
+++ stocks_dash.py
@@ -3,8 +3,6 @@ import yfinance as yf
 import plotly.graph_objects as go
 
 
-price=yf.Ticker(' ').history(period='1d', interval='15m').reset_index()
-
 app = Dash()
 
 app.layout = html.Div([
@@ -19,9 +17,9 @@ app.layout = html.Div([
         dcc.Tab(label='Candlestick Chart', 
                 children=dcc.Graph(id='stock-graph')),
         dcc.Tab(label = 'Recent Data', 
-                children=dash_table.DataTable(
-                    id='stock-data', 
-                    data=price.tail(10).to_dict('records'))),
+                children=[html.Div(id='latest-prices-div'),
+                          dash_table.DataTable(
+                          id='stock-data')])
     ]),
     dcc.Interval(id='chart-interval', interval=1000*60*15, n_intervals=0),
     dcc.Interval(id='table-interval', interval=1000*60, n_intervals=0)
@@ -50,6 +48,26 @@ def update_chart(button_click, chart_interval, stock_symbol):
                 low=price['Low'],
                 close=price['Close']))  
     return fig
+
+@app.callback(
+    Output('latest-prices-div', 'children'),
+    Output('stock-data', 'data'),
+    Input('submit-button', 'n_clicks'),
+    Input('table-interval', 'n_intervals'),
+    State('ticker-input', 'value')
+)
+
+def update_table(button_click, table_interval, stock_symbol):
+    if stock_symbol is None:
+        return '', []  # empty string for html div text and empty list for data
+    price=yf.Ticker(stock_symbol).history(period='1d', interval='1m').reset_index()
+    if len(price) == 0:
+        return f'No data for stock {stock_symbol} on Yahoo Finance', []
+    latest_price = price['Close'].iloc[-1]
+    latest_time = price['Datetime'].max().strftime('%b %d %Y %I:%M:%S %p')
+    data = price.tail(10).to_dict('records')
+    latest_price_info = f'The latest price is {latest_price} at the time of {latest_time}.'
+    return latest_price_info, data
 
 if __name__=='__main__':
     app.run_server(debug=True)
